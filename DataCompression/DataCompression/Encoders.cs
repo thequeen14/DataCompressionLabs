99c0c2359429700c6692a49aa553e5df96fc09f7
thequeen14 <flamina96@yandex.ru>
2017-05-09 18:57:08 +0300

    LZ78 finished, RLE done

--- DataCompression/DataCompression/Encoders.cs
+++ DataCompression/DataCompression/Encoders.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
@@ -29,7 +30,75 @@ namespace DataCompression
         public void Encode(string file)
         {
             var message = MessageExtractor.ExtractMessage(file);
+            var messageAfterBW = ApplyBWTransformation(message);
+            var messageAfterRLE = string.Empty;
+
+            int increment = 0;
+            for (int i = 0; i < message.Length; i += increment)
+            {
+                var currentCharacter = messageAfterBW.Key.Substring(i, 1);
+                increment = 0;
+                var nextCharacterPosition = i + increment + 1;
+                if (nextCharacterPosition < message.Length)
+                {
+                    while (messageAfterBW.Key.Substring(nextCharacterPosition, 1) == currentCharacter)
+                    {
+                        increment++;
+                        nextCharacterPosition++;
+                        if (nextCharacterPosition >= message.Length)
+                        {
+                            break;
+                        }
+                    }
+                }
+                
+                if (increment == 0)
+                {
+                    messageAfterRLE += currentCharacter;
+                    increment++;
+                }
+                else
+                {
+                    increment++;
+                    messageAfterRLE += currentCharacter + increment;
+                }
+            }
+            MessageFileWriter.WriteEncodedMessageToFile("output1.txt", messageAfterRLE);
         }
+
+        private KeyValuePair<string, int> ApplyBWTransformation(string text)
+        {
+            var table = GetBWTransformationTable(text);
+            var lastCharacters = string.Empty;
+            int textInTablePosition = -1;
+
+            for (int i = 0; i < table.Length; i++)
+            {
+                lastCharacters += table[i].Substring(table[i].Length - 1, 1);
+                if (table[i] == text)
+                {
+                    textInTablePosition = i;
+                }
+            }
+            return new KeyValuePair<string, int>(lastCharacters, textInTablePosition);
+        }
+
+        private string[] GetBWTransformationTable(string text)
+        {
+            var BWTable = new string[text.Length];
+            BWTable[0] = text;
+            for (int i = 1; i < text.Length; i++)
+            {
+                BWTable[i] = RotateWordByOneCharacter(BWTable[i - 1]);
+            }
+            Array.Sort(BWTable);
+            return BWTable;
+        }
+
+        private string RotateWordByOneCharacter(string word)
+        {
+            return word.Substring(1, word.Length - 1) + word[0];
+        } 
     }
 
     public class LZ78Encoder : IEncoder
@@ -44,20 +113,45 @@ namespace DataCompression
 
             for (int i = 0; i < message.Length; i += newEntrylength)
             {
+                newEntrylength = 1;
                 var newEntry = message.Substring(i, newEntrylength);
                 while (dictionary.ContainsKey(newEntry))
                 {
                     newEntrylength++;
                     newEntry = message.Substring(i, newEntrylength);
                 }
-                if (newEntry)
-                if (newEntrylength == 1 && !dictionary.ContainsKey(newEntry))
+                if (newEntrylength == 1)
                 {
-                    dictionary.Add(newEntry, dictionary.Count + 1);
-                    encodedMessage += newEntry;
+                    if (!dictionary.ContainsKey(newEntry))
+                    {
+                        dictionary.Add(newEntry, dictionary.Count + 1);
+                        encodedMessage += "0" + newEntry;
+                    }
+                    else
+                    {
+                        newEntrylength++;
+                        encodedMessage += dictionary[newEntry] + message[newEntrylength];
+                        dictionary.Add(message.Substring(i, newEntrylength), dictionary.Count + 1);
+                    }
                 }
                 else
+                {
+                    encodedMessage += dictionary[newEntry.Substring(0, newEntrylength - 1)] +
+                                      newEntry.Substring(newEntrylength - 1, 1);
+                    dictionary.Add(newEntry, dictionary.Count + 1);
+                }
             }
+            MessageFileWriter.WriteEncodedMessageToFile("output.txt", encodedMessage);
+        }
+    }
+
+    internal static class MessageFileWriter
+    {
+        internal static void WriteEncodedMessageToFile(string fileName, string text)
+        {
+            var sw = new StreamWriter(fileName);
+            sw.WriteLine(text);
+            sw.Close();
         }
     }
 
