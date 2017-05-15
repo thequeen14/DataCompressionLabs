fb35e492b147a3adcee1ca03ca07239919b4cc9a
Namioko <galaida_anna@inbox.ru>
2017-05-16 00:55:48 +0300

    Started HuffmanEncoder

---
+++ DataCompression/DataCompression/Huffman/HuffmanEncoder.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DataCompression.Huffman
+{
+    public class HuffmanEncoder : IEncoder
+    {
+        private readonly Dictionary<string, HuffmanTree> _frequencyTable = new Dictionary<string, HuffmanTree>();
+        private Dictionary<string, string> _codeTable = new Dictionary<string, string>(); 
+        private HuffmanTree _root;
+
+        public void Encode(string file)
+        {
+            var message = MessageExtractor.ExtractMessage(file);
+            GetFrequencyTable(message);
+
+            while(_frequencyTable.Count > 1)
+                AddNewLeaf();
+
+            if (_frequencyTable.Count == 1)
+            {
+                var tempLeft = _frequencyTable.ElementAt(0);
+                var newLeft = new HuffmanTree
+                {
+                    Frequency = tempLeft.Value.Frequency,
+                    Chars = tempLeft.Key
+                };
+                var temp = _root;
+                newLeft.LeftChild = temp;
+                _root = newLeft;
+            }
+
+            SetCodes();
+
+            var encodedMessage = MakeCode(message);
+            MessageFileWriter.WriteEncodedMessageToFile("HuffmanEncoderOutput.txt", encodedMessage);
+        }
+
+        private void GetFrequencyTable(string message)
+        {
+            foreach (var ch in message)
+            {
+                if (_frequencyTable.ContainsKey(ch.ToString())) //!!! tostring
+                    _frequencyTable[ch.ToString()].Frequency++; //!!! tostring
+                else
+                    _frequencyTable.Add(ch.ToString(), new HuffmanTree()); //!!! tostring
+            }
+            _frequencyTable.OrderByDescending(pair => pair.Value); //do something with ordering
+        }
+
+        private void AddNewLeaf()
+        {
+            var newLeft = new HuffmanTree();
+            var tempLeft = _frequencyTable.ElementAt(0);
+            newLeft.Frequency = tempLeft.Value.Frequency;
+            newLeft.Chars = tempLeft.Key;
+            _frequencyTable.Remove(newLeft.Chars);
+
+            var newRight = new HuffmanTree();
+            var tempRight = _frequencyTable.ElementAt(1);
+            newRight.Frequency = tempRight.Value.Frequency;
+            newRight.Chars = tempRight.Key;
+            _frequencyTable.Remove(newRight.Chars);
+
+            _root = new HuffmanTree(newLeft, newRight);
+            _root.Frequency = newLeft.Frequency + newRight.Frequency;
+            _root.Chars = newLeft.Chars + newRight.Chars;
+            _frequencyTable.Add(_root.Chars, _root);
+            _frequencyTable.OrderByDescending(pair => pair.Value); //do something with ordering
+        }
+
+        public void SetCodes()
+        {
+            _root.SetChildCodes();
+            _codeTable = _root.GetChilds(_codeTable);
+        }
+
+        public string MakeCode(string message)
+        {
+            string code = null;
+            foreach (var ch in message)
+            {
+                code += _codeTable[ch.ToString()]; //!!! tostring
+            }
+            return code;
+        }
+    }
+}
---
+++ DataCompression/DataCompression/Huffman/HuffmanTree.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataCompression.Huffman
+{
+
+
+    internal class HuffmanTree : IComparable
+    {
+        public HuffmanTree LeftChild { get; set; }
+        public HuffmanTree RightChild { get; set; }
+        public int Frequency { get; set; }
+        public string Chars { get; set; }
+        public string Code { get; set; }
+
+        public HuffmanTree()
+        {
+            Frequency = 1;
+            LeftChild = null;
+            RightChild = null;
+        }
+
+        public HuffmanTree(HuffmanTree left, HuffmanTree right)
+        {
+            Frequency = 1;
+            LeftChild = left;
+            RightChild = right;
+        }
+
+        public void SetChildCodes()
+        {
+            if (LeftChild != null)
+            {
+                LeftChild.Code = Code + "0";
+                LeftChild.SetChildCodes();
+            }
+
+            if (RightChild != null)
+            {
+                RightChild.Code = Code + "1";
+                RightChild.SetChildCodes();
+            }
+        }
+
+        public Dictionary<string, string> GetChilds(Dictionary<string, string> list)
+        {
+            if (LeftChild != null)
+                LeftChild.GetChilds(list);
+
+            if (RightChild != null)
+                RightChild.GetChilds(list);
+
+            if (LeftChild == null && RightChild == null)
+                list.Add(Chars, Code);
+            return list;
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj == null)
+                return -2;
+            var other = obj as HuffmanTree;
+            if (Frequency > other.Frequency)
+                return 1;
+            if (Frequency < other.Frequency)
+                return -1;
+            return 0;
+        }
+    }
+}
