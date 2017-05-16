72a7bb485fe9c7cc2efd99c8078d7d59ad5596ec
thequeen14 <flamina96@yandex.ru>
2017-05-16 18:47:00 +0300

    Binary tree added and used in FanoShennon

---
+++ DataCompression/DataCompression/FanoShennon/BinaryTree.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataCompression.FanoShennon
+{
+    public class Node<TKey, TValue>
+    {
+        public TKey Key { get; set; }
+        public TValue Value { get; set; }
+        public Node<TKey, TValue> Left { get; set; }
+        public Node<TKey, TValue> Right { get; set; }
+        public Node<TKey, TValue> Parent { get; set; }
+
+        public Node(TKey key, TValue value)
+        {
+            Key = key;
+            Value = value;
+            Left = null; //вообще говоря, они и по умолчанию null
+            Right = null;
+            Parent = null;
+        }
+    }
+
+    public class BinaryTree<TKey, TValue> where TKey : IComparable<TKey>
+    {
+        public int Count { get; private set; }
+        public Node<TKey, TValue> Root;
+
+        public BinaryTree()
+        {
+            Count = 0;
+            Root = null;
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            var node = new Node<TKey, TValue>(key, value);
+            if (Root == null)
+            {
+                Root = node;
+                Count = 1;
+            }
+            else
+            {
+                var tempNode = Root;
+                Node<TKey, TValue> parent = null;
+
+                while (tempNode != null)
+                {
+                    if (tempNode.Key.CompareTo(key) == 0)
+                    {
+                        throw new ArgumentException("Key must be unique.");
+                    }
+                    parent = tempNode;
+                    if (tempNode.Key.CompareTo(key) > 0)
+                    {
+                        tempNode = tempNode.Left;
+                    }
+                    else
+                    {
+                        tempNode = tempNode.Right;
+                    }
+                }
+
+                if (parent.Key.CompareTo(key) > 0)
+                {
+                    parent.Left = node;
+                }
+                else
+                {
+                    parent.Right = node;
+                }
+                node.Parent = parent;
+                Count++;
+            }
+        }
+
+        public int CountLeaves(Node<TKey, TValue> currentNode)
+        {
+            int count = 0;
+            if (currentNode.Left == null && currentNode.Right == null)
+            {
+                count++;
+            }
+            else
+            {
+                if (currentNode.Left != null)
+                {
+                    CountLeaves(currentNode.Left);
+                }
+                if (currentNode.Right != null)
+                {
+                    CountLeaves(currentNode.Right);
+                }
+            }
+            return count;
+        }
+    }
+}
--- DataCompression/DataCompression/FanoShennon/FanoShennonEncoder.cs
+++ DataCompression/DataCompression/FanoShennon/FanoShennonEncoder.cs
@@ -12,7 +12,47 @@ namespace DataCompression.FanoShennon
         {
             var message = MessageExtractor.ExtractMessage(file);
             var frequencyTable = SymbolFrequencyCounter.CountFrequenciesForLetters(message);
+            var tree = new BinaryTree<int, string>();
 
+            var charSequence = string.Empty;
+
+            foreach (var c in frequencyTable)
+            {
+                charSequence += c.Key;
+            }
+            tree.Add(message.Length, charSequence);
+
+            //отдельно выделить случай сообщения из одной повторяющейся буквы
+            var currentLevelNode = tree.Root;
+            while (tree.CountLeaves(tree.Root) != frequencyTable.Count)
+            {
+                var currentHalf = currentLevelNode.Key / 2;
+                var currentSum = 0;
+                var currentPositionOfDivision = 0;
+                var firstNodeString = string.Empty;
+
+                while (currentSum <= currentHalf)
+                {
+                    firstNodeString += frequencyTable.ElementAt(currentPositionOfDivision).Key;
+                    currentSum += frequencyTable.ElementAt(currentPositionOfDivision).Value;
+                    currentPositionOfDivision++;
+                }
+
+                var secondNodeString = currentLevelNode.Value.Substring(currentPositionOfDivision + 1,
+                    currentLevelNode.Value.Length - currentPositionOfDivision);
+
+                tree.Add(currentSum, firstNodeString);
+                tree.Add(currentLevelNode.Key - currentSum, secondNodeString);
+
+                if (currentLevelNode.Left.Value.Length != 1)
+                {
+                    currentLevelNode = currentLevelNode.Left;
+                }
+                else if (currentLevelNode.Right.Value.Length != 1)
+                {
+                    currentLevelNode = currentLevelNode.Right;
+                }
+            }
         }
     }
 }
