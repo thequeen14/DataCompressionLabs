e75cf7b103ce8b271e8bd4034f39a29710266f3e
thequeen14 <flamina96@yandex.ru>
2017-06-06 02:24:14 +0300

    Create LZ78Decoder.cs

---
+++ DataCompression/DataCompression/LZ78/LZ78Decoder.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataCompression.LZ78
+{
+    public class LZ78Decoder : IDecoder
+    {
+        public void Decode(string file)
+        {
+            var message = MessageExtractor.ExtractMessage(file);
+            var dictionary = new Dictionary<int, string>();
+            var decodedMessage = string.Empty;
+
+            int symbolPosition = 0;
+            int numberPosition = 0;
+            int numberLength = 0;
+            int newNumber = 0;
+
+            while (symbolPosition <= message.Length - 1)
+            {
+                while (char.IsDigit(message[numberPosition + numberLength]))
+                {
+                    numberLength++;
+                }
+
+                int.TryParse(message.Substring(numberPosition, numberLength), out newNumber);
+                symbolPosition = numberPosition + numberLength;
+
+                if (newNumber == 0)
+                {
+                    dictionary.Add(dictionary.Count + 1, message[symbolPosition].ToString());
+                    decodedMessage += message[symbolPosition];
+                }
+                else
+                {
+                    dictionary.Add(dictionary.Count + 1, dictionary[newNumber] + message[symbolPosition]);
+                }
+            }
+
+            MessageFileWriter.WriteEncodedMessageToFile("output.txt", decodedMessage);
+        }
+    }
+}
